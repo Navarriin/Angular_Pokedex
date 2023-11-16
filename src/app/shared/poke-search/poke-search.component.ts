c01b0657709b3a31f9a764136339b1d41f09e69d
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-16 18:06:45 -0300

    finalizando responsibilidades

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'poke-search',
   templateUrl: './poke-search.component.html',
-  styleUrls: ['./poke-search.component.scss'],
+  styleUrls: [
+    './poke-search.component.scss',
+    './poke-search.responsivity.component.scss',
+  ],
 })
 export class PokeSearchComponent {}
