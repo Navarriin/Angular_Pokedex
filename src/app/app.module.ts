e8ada215549c2bf45a947d46d7cc9f5e18b26e93
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-16 15:39:25 -0300

    começando a pegar os status da Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 // Meus Modulos
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PagesModule],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokeApiService {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get pegarPokemon(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      tap((res) => res),
+      tap((res) => {
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            (res) => (resPokemons.status = res)
+          );
+        });
+      })
+    );
+  }
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(map((res) => res));
+  }
 }
