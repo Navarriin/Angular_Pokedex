25035b4cfd16bb78ade12b0eec4eb16206a281e5
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-17 13:13:39 -0300

    criaçao de novo componente com export no modulo

---
+++ src/app/shared/poke-details/poke-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poke-details',
+  templateUrl: './poke-details.component.html',
+  styleUrls: ['./poke-details.component.scss'],
+})
+export class PokeDetailsComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 import { RouterModule } from '@angular/router';
+import { PokeDetailsComponent } from './poke-details/poke-details.component';
 
 @NgModule({
-  declarations: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
-  exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
+  declarations: [
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent,
+    PokeDetailsComponent,
+  ],
+  exports: [
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent,
+    PokeDetailsComponent,
+  ],
   imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
