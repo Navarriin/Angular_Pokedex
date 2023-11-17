638dc757972672244d898bc9f87123d8a022e930
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-17 16:15:14 -0300

    ajustando componentes

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent {
+  private url: string = environment.apiComId;
+  private urlName: string = environment.apiName;
 
+  public pokemon: any;
+
+  constructor(
+    private activetedRoute: ActivatedRoute,
+    private pokeApi: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  getPokemon(): any {
+    const id = this.activetedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApi.apiGetPokemons(`${this.url}/${id}`);
+    const name = this.pokeApi.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      this.pokemon = res;
+    });
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,21 +4,10 @@ import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 import { RouterModule } from '@angular/router';
-import { PokeDetailsComponent } from './poke-details/poke-details.component';
 
 @NgModule({
-  declarations: [
-    PokeHeaderComponent,
-    PokeSearchComponent,
-    PokeListComponent,
-    PokeDetailsComponent,
-  ],
-  exports: [
-    PokeHeaderComponent,
-    PokeSearchComponent,
-    PokeListComponent,
-    PokeDetailsComponent,
-  ],
+  declarations: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
+  exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
   imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
